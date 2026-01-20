1734d3598e21d2543a8bab1b69069697de885886
Dan Electronics <SimpleElectronicsYT@gmail.com>
2026-01-20 07:29:27 -0500

    progress

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-from logger import log_state
+from logger import log_state, log_event
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+import sys
 
 def main():
     pygame.init()
@@ -28,6 +29,12 @@ def main():
                 return
         screen.fill("black")
         updatable.update(dt)
+        for asteroid in asteroids:
+            if player.collides_with(asteroid):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+            
         for to_draw in drawable:
             to_draw.draw(screen)
         pygame.display.flip()
